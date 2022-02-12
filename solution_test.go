a0d59617ea4f85f181b85c3705f6d6981bcb2936
Nurlan42 <ikhsan.nurlan@gmail.com>
2022-02-12 16:37:08 +0600

    added test file

---
+++ solution_test.go
@@ -0,0 +1,13 @@
+package solutions
+
+import "testing"
+
+func TestGetMessage(t *testing.T) {
+	expected := string([]rune{72, 101, 108, 108, 111, 32, 128506, 65039, 32, 33})
+
+	got := GetMessage()
+
+	if got != expected {
+		t.Errorf("GetMessage() = %v, expected: %v\n", got, expected)
+	}
+}
